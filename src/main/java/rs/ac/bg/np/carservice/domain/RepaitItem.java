0658eefa0793de089a723355a1dedf5a4c267a50
Luka <tatovicluka@gmail.com>
2024-02-12 16:45:17 +0100

    Added javadoc for domain class RepaitItem.

--- src/main/java/rs/ac/bg/np/carservice/domain/RepaitItem.java
+++ src/main/java/rs/ac/bg/np/carservice/domain/RepaitItem.java
@@ -3,30 +3,64 @@ package rs.ac.bg.np.carservice.domain;
 import jakarta.persistence.*;
 
 import java.util.Objects;
+/**
+ * Entitet u bazi podataka/domenska klasa stavki popravke
+ *
+ * Koristi se da predstavi vezu sa tabelom "repait_item" iz baze podataka i kao domenski objekat.
+ * Pamti sve bitne informacije o stavci popravke to jest ID stavke popravke, popravku za koju je stavka i deo koji se popravlja.
+ *
+ * @author Luka
+ */
 
 @Entity
 public class RepaitItem {
+    /**
+     * Automatski generisan identifikator stavke popravke koji je unikatan i identifikuje objekat u bazi podataka.
+     */
     @Id
     @GeneratedValue
     private long repaiItemId;
+    /**
+     * Popravka koja sadrzi stavku popravke koja je tipa Repair
+     */
     @ManyToOne
     private Repair repair;
+    /**
+     * Deo koji se popravlja koji je tipa Part
+     */
     @OneToOne
     private Part part;
-
+    /**
+     * Bezparametarski konstruktor koji inicijalizuje objekat klase RepaitItem.
+     */
     public RepaitItem() {
     }
 
+    /**
+     * Parametarski konstruktor koji inicijalizuje objekat klase RepaitItem sa zadatim vrednostima
+     *
+     * @param repaiItemId ID stavke popravke.
+     * @param repair popravka koja sadrzi stavku popravke.
+     * @param part deo koji se popravlja.
+     */
     public RepaitItem(long repaiItemId, Repair repair, Part part) {
         setRepaiItemId(repaiItemId);
         setRepair(repair);
         setPart(part);
     }
-
+    /**
+     * Vraca ID stavke popravke.
+     * @return repaiItemId kao long.
+     */
     public long getRepaiItemId() {
         return repaiItemId;
     }
 
+    /**
+     * Postavlja repaiItemId na zadatu vrednost.
+     * @param repaiItemId ID stavke popravke.
+     * @throws IllegalArgumentException ako je repaiItemId manji od 1.
+     */
     public void setRepaiItemId(long repaiItemId) {
         if(repaiItemId>0) {
             this.repaiItemId = repaiItemId;
@@ -35,10 +69,19 @@ public class RepaitItem {
         }
     }
 
+    /**
+     * Vraca popravku koja sadrzi stavku popravke.
+     * @return repair kao Repair.
+     */
     public Repair getRepair() {
         return repair;
     }
 
+    /**
+     * Postavlja popravku na zadatu vrednost.
+     * @param repair popravka koja sadrzi stavku popravke.
+     * @throws IllegalArgumentException ako je popravka null.
+     */
     public void setRepair(Repair repair) {
         if(repair!=null) {
             this.repair = repair;
@@ -47,10 +90,19 @@ public class RepaitItem {
         }
     }
 
+    /**
+     * Vraca deo koji se popravlja.
+     * @return part kao Part.
+     */
     public Part getPart() {
         return part;
     }
 
+    /**
+     * Postavlja deo na zadatu vrednost.
+     * @param part deo koji se popravlja.
+     * @throws IllegalArgumentException ako je deo null.
+     */
     public void setPart(Part part) {
         if(part!=null) {
             this.part = part;
@@ -58,7 +110,15 @@ public class RepaitItem {
             throw new IllegalArgumentException("Deo ne moze biti null");
         }
     }
-
+    /**
+     * Poredi dve stavke popravke po ID-ju.
+     * @param o koji predstavlja stavku popravke.
+     * @return
+     * <ul>
+     *      <li> true  - ako je repaiItemId obe stavke popravke isti ili ako je unet isti objekat </li>
+     *      <li> false - ako je unet null objekat ili ako nije klase RepaitItem</li>
+     * </ul>
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -66,7 +126,11 @@ public class RepaitItem {
         RepaitItem that = (RepaitItem) o;
         return repaiItemId == that.repaiItemId;
     }
-
+    /**
+     * Izracunava hash code na osnovu repaiItemId.
+     *
+     * @return hash code na osnovu repaiItemId.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(repaiItemId);
