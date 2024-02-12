ef907e53534d4e929a696581d933f1204202541d
Luka <tatovicluka@gmail.com>
2024-02-12 17:31:13 +0100

    Added javadoc for service class SuplierService.

--- src/main/java/rs/ac/bg/np/carservice/service/SuplierService.java
+++ src/main/java/rs/ac/bg/np/carservice/service/SuplierService.java
@@ -7,16 +7,35 @@ import rs.ac.bg.np.carservice.repository.SuplierRepository;
 
 import java.util.List;
 import java.util.Optional;
+/**
+ * Sadrzi poslovnu logiku rada sa dobavljacem.
+ * Klasa sluzi da manipulise i upravlja modelom i podacima vezanim sa dobavljacem.
+ *
+ * Omogucava vracanje svih dobavljaca, pravljenje novog dobavljaca i brisanje dobavljaca.
+ * @author Luka
+ */
 
 @Service
 public class SuplierService {
-
+    /**
+     * Broker baze podataka koji je posrednik ka tabeli suplier.
+     */
     @Autowired
     private SuplierRepository suplierRepository;
+    /**
+     * Metoda koja vraca listu svih dobavljaca.
+     * @return List<Suplier> lista svih dobavljaca.
+     */
     public List<Suplier> getAll() {
         return suplierRepository.findAll();
     }
-
+    /**
+     * Metoda za perzistiranje dobavljaca u sistem kroz trajno cuvanje.
+     * Novi dobavljac se cuva u bazi podataka.
+     * @param suplier dobavljaca koji se treba perzistirati.
+     * @return perzistirani dobavljac.
+     * @throws Exception ako dobavljac sa istom adresom postoji u bazi podataka.
+     */
     public Suplier addNewSuplier(Suplier suplier) throws Exception{
         Optional<Suplier> optionalSuplier= suplierRepository.findByAdress(suplier.getAdress());
         if(optionalSuplier.isPresent()){
@@ -25,6 +44,10 @@ public class SuplierService {
         return  suplierRepository.save(suplier);
     }
 
+    /**
+     * Metoda koja brise dobavljaca sa odredjenim ID-jem iz baze.
+     * @param suplierId ID dobavljaca koji se brise.
+     */
     public void deleteSuplier(long suplierId) {
         suplierRepository.deleteById(suplierId);
     }
