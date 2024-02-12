aca60460517786075b4057fb0de8a16dd9a1dbf9
Luka <tatovicluka@gmail.com>
2024-02-12 17:16:55 +0100

    Added javadoc for service class OwnerService.

--- src/main/java/rs/ac/bg/np/carservice/service/OwnerService.java
+++ src/main/java/rs/ac/bg/np/carservice/service/OwnerService.java
@@ -6,11 +6,30 @@ import rs.ac.bg.np.carservice.domain.Owner;
 import rs.ac.bg.np.carservice.repository.OwnerRepository;
 
 import java.util.Optional;
+/**
+ * Sadrzi poslovnu logiku rada sa vlasnikom.
+ * Klasa sluzi da manipulise i upravlja modelom i podacima vezanim sa vlasnikom.
+ *
+ * Omogucava pravljenje novog vlasnika i vracanje pojedinacnog vlasnika.
+ *
+ */
+
 
 @Service
 public class OwnerService {
+    /**
+     * Broker baze podataka koji je posrednik ka tabeli owner.
+     */
     @Autowired
     private OwnerRepository ownerRepository;
+
+    /**
+     * Metoda za perzistiranje vlasnika u sistem kroz trajno cuvanje.
+     * Novi vlasnik se cuva u bazi podataka.
+     * @param owner vlasnik koji se treba perzistirati.
+     * @return perzistirani vlasnik.
+     * @throws Exception ako vlasnik sa istim brojem telefona postoji u bazi podataka.
+     */
     public Owner createOwner(Owner owner) throws Exception {
         Optional<Owner> optionalOwner= ownerRepository.findByPhoneNumber(owner.getPhoneNumber());
         if(optionalOwner.isPresent()){
@@ -19,6 +38,12 @@ public class OwnerService {
         return ownerRepository.save(owner);
     }
 
+    /**
+     * Metoda koja vraca vlasnika sa odredjenim ID-jem iz baze podatka.
+     * @param ownerId ID vlasnika koji se trazi.
+     * @return vlasnik iz baze sa odredjenim ID-jem.
+     * @throws Exception ako vlasnik sa odredjenim ID-jem ne postoji u bazi podataka.
+     */
     public Owner getOneOwner(long ownerId) throws Exception{
         Optional<Owner> optionalOwner= ownerRepository.findById(ownerId);
         if(!optionalOwner.isPresent()){
