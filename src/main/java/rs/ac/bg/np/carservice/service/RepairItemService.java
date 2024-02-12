8677357be18f137650b212995ada15366f141428
Luka <tatovicluka@gmail.com>
2024-02-12 23:00:51 +0100

    Added javadoc for service class RepairItemService.

--- src/main/java/rs/ac/bg/np/carservice/service/RepairItemService.java
+++ src/main/java/rs/ac/bg/np/carservice/service/RepairItemService.java
@@ -13,19 +13,46 @@ import rs.ac.bg.np.carservice.repository.RepairRepository;
 import java.util.List;
 import java.util.Optional;
 import java.util.regex.Pattern;
-
+/**
+ * Sadrzi poslovnu logiku rada sa stavkom popravke.
+ * Klasa sluzi da manipulise i upravlja modelom i podacima vezanim sa stavkom popravke.
+ *
+ * Omogucava vracanje svih stavki popravke, pravljenje nove stavke popravke i povezivanje popravke sa stavkom popravke.
+ * @author Luka
+ */
 @Service
 public class RepairItemService {
+    /**
+     * Broker baze podataka koji je posrednik ka tabeli repait_item.
+     */
     @Autowired
     private RepairItemRepository repairItemRepository;
+    /**
+     * Broker baze podataka koji je posrednik ka tabeli repair.
+     */
     @Autowired
     private RepairRepository repairRepository;
+    /**
+     * Broker baze podataka koji je posrednik ka tabeli part.
+     */
     @Autowired
     private PartRepository partRepository;
+    /**
+     * Metoda koja vraca listu svih stavki popravke.
+     * @return List<RepaitItem> lista svih stavki popravke.
+     */
     public List<RepaitItem> getAllRepairItems() {
         return repairItemRepository.findAll();
     }
 
+    /**
+     * Metoda za perzistiranje stavke popravke u sistem kroz trajno cuvanje.
+     * Nova stavka popravke se cuva u bazi podataka.
+     * @param repaitItem stavka popravke koja se treba perzistirati.
+     * @param partId ID dela na koji se odnosi stavka popravke.
+     * @return perzistirana stavka poprvake.
+     * @throws Exception ako ne postoji deo sa odredjenim ID-jem ili vec postoji stavka popravke u bazi podataka.
+     */
     public RepaitItem addNewRepairtItem(RepaitItem repaitItem, long partId) throws Exception{
        // Optional<RepaitItem> optionalRepaitItem= repairItemRepository.
        Optional<Part> optionalPart= partRepository.findById(partId);
@@ -42,6 +69,13 @@ public class RepairItemService {
 
     }
 
+    /**
+     * Metoda koja vraca stavku popravke zajedno sa popravkom sa kojom je povezana.
+     * @param repairitemId ID stavke popravke
+     * @param repairId ID stavke sa kojom ce se stavka popravke povezati.
+     * @return stavka popravke sa popravkom
+     * @throws Exception ako ne postoji stavka ili popravka sa odredjenim ID-jem, ako je stavka popravke vec navedena u popravci, ako deo stavke popravke ne moze da se stavi na automobil koji se popravja znog pogresne marke ili modela
+     */
     public RepaitItem addRepairForRepairItem(long repairitemId, long repairId) throws Exception{
         Optional<RepaitItem> optionalRepaitItem= repairItemRepository.findById(repairitemId);
         if(!optionalRepaitItem.isPresent()){
