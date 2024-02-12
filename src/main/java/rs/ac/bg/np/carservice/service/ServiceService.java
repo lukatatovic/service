372747262ad22cff5c097e711e3730e8a84f7b79
Luka <tatovicluka@gmail.com>
2024-02-12 17:35:04 +0100

    Added javadoc for service class ServiceService.

--- src/main/java/rs/ac/bg/np/carservice/service/ServiceService.java
+++ src/main/java/rs/ac/bg/np/carservice/service/ServiceService.java
@@ -6,10 +6,27 @@ import rs.ac.bg.np.carservice.domain.Servicer;
 import rs.ac.bg.np.carservice.repository.ServiceRepository;
 
 import java.util.Optional;
+/**
+ * Sadrzi poslovnu logiku rada sa servisom.
+ * Klasa sluzi da manipulise i upravlja modelom i podacima vezanim sa servisom.
+ *
+ * Omogucava pravljenje novog servisa i vracanje pojedinacnog servisa.
+ * @author Luka
+ */
 @org.springframework.stereotype.Service
 public class ServiceService {
+    /**
+     * Broker baze podataka koji je posrednik ka tabeli service.
+     */
     @Autowired
     private ServiceRepository serviceRepository;
+    /**
+     * Metoda za perzistiranje servisa u sistem kroz trajno cuvanje.
+     * Novi servis se cuva u bazi podataka.
+     * @param service servis koji se treba perzistirati.
+     * @return perzistirani servis.
+     * @throws Exception ako servis sa istim nazivom postoji u bazi podataka.
+     */
     public Service createService(Service service) throws Exception{
         Optional<Service> optionalService= serviceRepository.findByName(service.getName());
         if(optionalService.isPresent()){
@@ -17,7 +34,12 @@ public class ServiceService {
         }
         return  serviceRepository.save(service);
     }
-
+    /**
+     * Metoda koja vraca servis sa odredjenim ID-jem iz baze podatka.
+     * @param serviceId ID servisa koji se trazi.
+     * @return servis iz baze sa odredjenim ID-jem.
+     * @throws Exception ako servis sa odredjenim ID-jem ne postoji u bazi podataka.
+     */
     public Service getOneService(long serviceId) throws Exception {
         Optional<Service> optionalService= serviceRepository.findById(serviceId);
         if(!optionalService.isPresent()){
